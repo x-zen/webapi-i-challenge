ff5be466d8eca79db420b7c3a830cc27917ec2f9
x-zen <jdharris.x@gmail.com>
2019-04-02 04:08:29 -0500

    Base node-express server setup

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// implement your API here
+const express = require('express') // import the express package
+
+const server = express(); // creates the server
+
+// handle request to the root of the api, the '/' route
+server.get('/', (req, res) => {
+  res.send('Howdy from x-zen');
+});
+
+// watch for connections on port 3000
+server.listen(3000, () =>
+  console.log('Server running on http://localhost:3000')
+);
